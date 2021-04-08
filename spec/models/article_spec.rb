ded16123fa0cdcba44817864b2e14fa4751587c6
juliazo <davincy@gmail.com>
2021-04-08 13:43:19 -0500

    Validate article :title

---
+++ spec/models/article_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
+ describe '#validations' do
+
+ 	let(:article) { build(:article) }
+
+ 	it "test factory is valid" do
+ 		expect(article).to be_valid
+ 	end
+
+ 	it "has an invalid title" do
+ 		article.title = ''
+ 		expect(article).not_to be_valid
+ 		expect(article.errors[:title]).to include("can't be blank")
+ 	end
+ 
+  end
+end
