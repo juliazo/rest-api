56830870593513d1c0e1b8dd3c2b3882a810ab7e
juliazo <davincy@gmail.com>
2021-04-08 18:06:05 -0500

    format index to json.api

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,12 @@
+class ArticlesController < ApplicationController
+	def index
+		articles = Article.all
+		render json: serializer.new(articles), status: :ok
+    end
+
+    def serializer
+    	ArticleSerializer
+    end
+    def show
+    end
+end
---
+++ spec/requests/articles_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+RSpec.describe ArticlesController do
+	describe "#index" do
+		it "returns a success response" do
+			get "/articles"
+			expect(response).to have_http_status(:ok)
+		end
+		it "returns proper json" do
+			article = create(:article)
+			get "/articles"
+			aggregate_failures do
+				expect(json_data.length).to eq(1)
+				expected = json_data.first
+				expect(expected[:id]).to eq(article.id.to_s)
+				expect(expected[:type]).to eq('article')
+				expect(expected[:attributes]).to eq(
+							title: article.title,
+							content: article.content,
+							slug: article.slug
+					)
+			end
+		end
+	end
+end
