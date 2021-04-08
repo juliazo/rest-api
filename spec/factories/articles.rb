c4b50ac4f5fd18001ee86eb8971fcb9f578ab41f
juliazo <davincy@gmail.com>
2021-04-08 14:52:09 -0500

    validate article :slug, :content

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :article do
     title { "Sample article" }
     content { "sample content" }
-    slug { "sample-article" }
+    slug {"sample-slug"}
   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Article, type: :model do
  describe '#validations' do
 
- 	let(:article) { build(:article) }
+ 	let(:article) { create(:article) }
 
  	it "test factory is valid" do
  		expect(article).to be_valid
@@ -15,5 +15,25 @@ RSpec.describe Article, type: :model do
  		expect(article.errors[:title]).to include("can't be blank")
  	end
  
+ 	it "has an invalid content" do
+ 		article.content = ''
+ 		expect(article).not_to be_valid
+ 		expect(article.errors[:content]).to include("can't be blank")
+ 	end
+
+ 	it "has an invalid slug" do
+ 		article.slug = ''
+ 		expect(article).not_to be_valid
+ 		expect(article.errors[:slug]).to include("can't be blank")
+ 	end
+
+ 	it "has uniqueness slug" do
+ 		article1 = create(:article)
+        expect(article1).to be_valid
+        article2 = build(:article, slug: article1.slug)
+        expect(article2).not_to be_valid
+        expect(article2.errors[:slug]).to include('has already been taken')
+ 	end
+
   end
 end
